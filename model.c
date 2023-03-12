a92e9f0fb45ee3d8be6be9ca233c7c11cd8f9cfa
Traftmine <leo.royer.1@etu.u-bordeaux.fr>
2023-03-12 16:18:09 +0100

    ajout clic pour placer le 1 & un peu de render

--- model.c
+++ model.c
@@ -46,7 +46,7 @@ Env *init(SDL_Window *win, SDL_Renderer *ren, int argc, char *argv[]) {
   TTF_Font* font = TTF_OpenFont(FONT, FONTSIZE);
   if (!font) ERROR("TTF_OpenFont: %s\n", FONT);
   TTF_SetFontStyle(font, TTF_STYLE_BOLD);                                   // TTF_STYLE_ITALIC | TTF_STYLE_NORMAL
-  SDL_Surface* surf = TTF_RenderText_Blended(font, "Hello World!", color);  // blended rendering for ultra nice text
+  SDL_Surface* surf = TTF_RenderText_Blended(font, "Takuzu", color);        // blended rendering for ultra nice text
   env->text = SDL_CreateTextureFromSurface(ren, surf);
   SDL_FreeSurface(surf);
   TTF_CloseFont(font);
@@ -57,7 +57,20 @@ Env *init(SDL_Window *win, SDL_Renderer *ren, int argc, char *argv[]) {
 /* **************************************************************** */
 
 void render(SDL_Window *win, SDL_Renderer *ren, Env *env) {
-    /* PUT YOUR CODE HERE TO RENDER TEXTURES, ... */
+    SDL_Rect rect;
+
+  /* get current window size */
+  int w, h;
+  SDL_GetWindowSize(win, &w, &h);
+
+  /* render background texture */
+  SDL_RenderCopy(ren, env->background, NULL, NULL); /* stretch it */
+
+  /* render text texture */
+  SDL_QueryTexture(env->text, NULL, NULL, &rect.w, &rect.h);
+  rect.x = w / 2 - rect.w / 2;
+  rect.y = h / 2 - rect.h / 2;
+  SDL_RenderCopy(ren, env->text, NULL, &rect);
 }
 
 /* **************************************************************** */
@@ -68,8 +81,30 @@ bool process(SDL_Window *win, SDL_Renderer *ren, Env *env, SDL_Event *e) {
   }
 
   /* PUT YOUR CODE HERE TO PROCESS EVENTS */
+    int w, h;
+  SDL_GetWindowSize(win, &w, &h);
 
-  return false;
+  /* generic events */
+  if (e->type == SDL_QUIT) {
+    return true;
+  }
+  /* Android events */
+#ifdef __ANDROID__
+  else if (e->type == SDL_FINGERDOWN) {
+    env->mario_x = e->tfinger.x * w; /* tfinger.x, normalized in [0..1] */
+    env->mario_y = e->tfinger.y * h; /* tfinger.y, normalized in [0..1] */
+  }
+  /* other events */
+#else
+  else if (e->type == SDL_MOUSEBUTTONDOWN) {
+    SDL_Point mouse;
+    SDL_GetMouseState(&mouse.x, &mouse.y);
+    env->one = mouse.x;
+    env->one = mouse.y;
+  }
+#endif
+
+  return false; /* don't quit */
 }
 
 /* **************************************************************** */
