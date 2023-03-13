7e6e39fff75cc0fa5430e2848f7ffbd899c4c42c
Traftmine <leo.royer.1@etu.u-bordeaux.fr>
2023-03-13 10:02:26 +0100

    modification du cmake

---
+++ game_sdl.c
@@ -0,0 +1,121 @@
+#include "game_sdl.h"
+
+#include <SDL.h>
+#include <SDL_image.h>  // required to load transparent texture from PNG
+#include <SDL_ttf.h>    // required to use TTF fonts
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* **************************************************************** */
+
+#define FONT "RobotCrush.ttf"
+#define FONTSIZE 36
+#define BACKGROUND "background.png"
+#define ONE "one.png"
+#define ZERO "zero.png"
+
+/* **************************************************************** */
+
+struct Env_t {
+  SDL_Texture* background;
+  SDL_Texture* one;         // S_ONE logo
+  SDL_Texture* zero;        // S_ZERO logo
+  // how to do a grid ?
+};
+
+/* **************************************************************** */
+
+Env *init(SDL_Window *win, SDL_Renderer *ren, int argc, char *argv[]) {
+  Env *env = malloc(sizeof(struct Env_t));
+
+  /* init background texture from PNG image */
+  env->background = IMG_LoadTexture(ren, BACKGROUND);
+  if (!env->background) ERROR("IMG_LoadTexture: %s\n", BACKGROUND);
+
+  /* init S_ONE texture from PNG image */
+  env->one = IMG_LoadTexture(ren, ONE);
+  if (!env->one) ERROR("IMG_LoadTexture: %s\n", ONE);
+
+  /* init S_ZERO texture from PNG image */
+  env->zero = IMG_LoadTexture(ren, ZERO);
+  if (!env->zero) ERROR("IMG_LoadTexture: %s\n", ZERO);
+
+  /* init text texture using Arial font */
+  SDL_Color color = {0, 0, 0, 0.8}; /* light balck color in RGBA */
+  TTF_Font* font = TTF_OpenFont(FONT, FONTSIZE);
+  if (!font) ERROR("TTF_OpenFont: %s\n", FONT);
+  TTF_SetFontStyle(font, TTF_STYLE_BOLD);                                   // TTF_STYLE_ITALIC | TTF_STYLE_NORMAL
+  SDL_Surface* surf = TTF_RenderText_Blended(font, "Takuzu", color);        // blended rendering for ultra nice text
+  env->text = SDL_CreateTextureFromSurface(ren, surf);
+  SDL_FreeSurface(surf);
+  TTF_CloseFont(font);
+
+  return env;
+}
+
+/* **************************************************************** */
+
+void render(SDL_Window *win, SDL_Renderer *ren, Env *env) {
+    SDL_Rect rect;
+
+  /* get current window size */
+  int w, h;
+  SDL_GetWindowSize(win, &w, &h);
+
+  /* render background texture */
+  SDL_RenderCopy(ren, env->background, NULL, NULL); /* stretch it */
+
+  /* render text texture */
+  SDL_QueryTexture(env->text, NULL, NULL, &rect.w, &rect.h);
+  rect.x = w / 2 - rect.w / 2;
+  rect.y = h / 2 - rect.h / 2;
+  SDL_RenderCopy(ren, env->text, NULL, &rect);
+}
+
+/* **************************************************************** */
+
+bool process(SDL_Window *win, SDL_Renderer *ren, Env *env, SDL_Event *e) {
+  if (e->type == SDL_QUIT) {
+    return true;
+  }
+
+  /* PUT YOUR CODE HERE TO PROCESS EVENTS */
+    int w, h;
+  SDL_GetWindowSize(win, &w, &h);
+
+  /* generic events */
+  if (e->type == SDL_QUIT) {
+    return true;
+  }
+  /* Android events */
+#ifdef __ANDROID__
+  else if (e->type == SDL_FINGERDOWN) {
+    env->mario_x = e->tfinger.x * w; /* tfinger.x, normalized in [0..1] */
+    env->mario_y = e->tfinger.y * h; /* tfinger.y, normalized in [0..1] */
+  }
+  /* other events */
+#else
+  else if (e->type == SDL_MOUSEBUTTONDOWN) {
+    SDL_Point mouse;
+    SDL_GetMouseState(&mouse.x, &mouse.y);
+    env->one = mouse.x;
+    env->one = mouse.y;
+  }
+#endif
+
+  return false; /* don't quit */
+}
+
+/* **************************************************************** */
+
+void clean(SDL_Window *win, SDL_Renderer *ren, Env *env) {
+  SDL_DestroyTexture(env->background);
+  SDL_DestroyTexture(env->zero);
+  SDL_DestroyTexture(env->one);
+  SDL_DestroyTexture(env->text);
+
+  free(env);
+}
+
+/* **************************************************************** */
